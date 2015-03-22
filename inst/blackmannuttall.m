e6e490b9e6dffc15a054819eb429df465299e662
Mike Miller <mtmiller@octave.org>
2015-03-21 21:07:04 -0400

    Support "periodic" window function types (bug #44344)
    * blackmanharris.m, blackmannuttall.m, hann.m, nuttallwin.m: Implement
    the "periodic" and "symmetric" window type options.  Add %!tests.
    * flattopwin.m, welchwin.m: Make style and docstrings consistent with
    other window functions.  Add %!tests.

--- inst/blackmannuttall.m
+++ inst/blackmannuttall.m
@@ -14,37 +14,59 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {} blackmannuttall (@var{m})
+## @deftypefn  {Function File} {} blackmannuttall (@var{m})
+## @deftypefnx {Function File} {} blackmannuttall (@var{m}, "periodic")
+## @deftypefnx {Function File} {} blackmannuttall (@var{m}, "symmetric")
 ## Return the filter coefficients of a Blackman-Nuttall window of length
 ## @var{m}.
+##
+## If the optional argument @code{"periodic"} is given, the periodic form
+## of the window is returned.  This is equivalent to the window of length
+## @var{m}+1 with the last coefficient removed.  The optional argument
+## @code{"symmetric"} is equivalent to not specifying a second argument.
+##
 ## @seealso{nuttallwin, kaiser}
 ## @end deftypefn
 
-function w = blackmannuttall (m)
+function w = blackmannuttall (m, opt)
 
-  if (nargin != 1)
+  if (nargin < 1 || nargin > 2)
     print_usage ();
   elseif (! (isscalar (m) && (m == fix (m)) && (m > 0)))
     error ("blackmannuttall: M must be a positive integer");
   endif
 
+  N = m - 1;
+  if (nargin == 2)
+    switch (opt)
+      case "periodic"
+        N = m;
+      case "symmetric"
+        N = m - 1;
+      otherwise
+        error ("blackmannuttall: window type must be either \"periodic\" or \"symmetric\"");
+    endswitch
+  endif
+
   if (m == 1)
     w = 1;
   else
-    N = m - 1;
     a0 = 0.3635819;
     a1 = 0.4891775;
     a2 = 0.1365995;
     a3 = 0.0106411;
-    n = 0:N;
+    n = [0:m-1]';
     w = a0 - a1.*cos(2.*pi.*n./N) + a2.*cos(4.*pi.*n./N) - a3.*cos(6.*pi.*n./N);
-    w = w.';
   endif
 
 endfunction
 
 %!assert (blackmannuttall (1), 1)
 %!assert (blackmannuttall (2), 0.0003628 * ones (2, 1), eps)
+%!assert (blackmannuttall (15), flipud (blackmannuttall (15)), 10*eps);
+%!assert (blackmannuttall (16), flipud (blackmannuttall (16)), 10*eps);
+%!assert (blackmannuttall (15), blackmannuttall (15, "symmetric"));
+%!assert (blackmannuttall (16)(1:15), blackmannuttall (15, "periodic"));
 
 %% Test input validation
 %!error blackmannuttall ()
@@ -52,3 +74,5 @@ endfunction
 %!error blackmannuttall (-1)
 %!error blackmannuttall (ones (1, 4))
 %!error blackmannuttall (1, 2)
+%!error blackmannuttall (1, "invalid")
+
--- inst/hann.m
+++ inst/hann.m
@@ -14,8 +14,15 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {} hann (@var{m})
+## @deftypefn  {Function File} {} hann (@var{m})
+## @deftypefnx {Function File} {} hann (@var{m}, "periodic")
+## @deftypefnx {Function File} {} hann (@var{m}, "symmetric")
 ## Return the filter coefficients of a Hanning window of length @var{m}.
+##
+## If the optional argument @code{"periodic"} is given, the periodic form
+## of the window is returned.  This is equivalent to the window of length
+## @var{m}+1 with the last coefficient removed.  The optional argument
+## @code{"symmetric"} is equivalent to not specifying a second argument.
 ##
 ## This function exists for @sc{matlab} compatibility only, and is equivalent
 ## to @code{hanning (@var{m})}.
@@ -23,27 +30,35 @@
 ## @seealso{hanning}
 ## @end deftypefn
 
-function w = hann (m)
+function w = hann (varargin)
 
-  if (nargin != 1)
+  if (nargin < 1 || nargin > 2)
     print_usage ();
   endif
 
-  w = hanning (m);
+  w = hanning (varargin{:});
 
 endfunction
 
 %!assert (hann (1), 1);
 %!assert (hann (2), zeros (2, 1));
-%!assert (hann (16), fliplr (hann (16)));
-%!assert (hann (15), fliplr (hann (15)));
+%!assert (hann (16), flipud (hann (16)), 10*eps);
+%!assert (hann (15), flipud (hann (15)), 10*eps);
 %!test
 %! N = 15;
 %! A = hann (N);
 %! assert (A(ceil (N/2)), 1);
 
+%!assert (hann (15), hann (15, "symmetric"));
+%!assert (hann (16)(1:15), hann (15, "periodic"));
+%!test
+%! N = 16;
+%! A = hann (N, "periodic");
+%! assert (A (N/2 + 1), 1);
+
 %% Test input validation
 %!error hann ()
 %!error hann (0.5)
 %!error hann (-1)
-%!error hann (1, 2)
+%!error hann (1, "invalid")
+
