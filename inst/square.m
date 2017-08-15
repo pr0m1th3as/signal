79ef8305c32e25566d28836bf09b3416a8876b84
Juan Pablo Carbajal <ajuanpi+dev@gmail.com>
2017-08-15 21:16:50 +0200

    fix bug #51673 with partial patch by Thomas Dean.

--- inst/square.m
+++ inst/square.m
@@ -6,11 +6,11 @@
 ## @deftypefnx {Function File} {@var{s} =} square (@var{t})
 ## Generate a square wave of period 2 pi with limits +1/-1.
 ##
-## If @var{duty} is specified, the square wave is +1 for that portion of the
-## time.
+## If @var{duty} is specified, it is the percentage of time the square
+## wave is "on".  The square wave is +1 for that portion of the time.
 ##
 ## @verbatim
-##                     on time
+##                   on time * 100
 ##    duty cycle = ------------------
 ##                 on time + off time
 ## @end verbatim
@@ -18,13 +18,14 @@
 ## @seealso{cos, sawtooth, sin, tripuls}
 ## @end deftypefn
 
-function v = square (t, duty = 0.5)
+function v = square (t, duty = 50)
 
   if (nargin < 1 || nargin > 2)
     print_usage;
   endif
+  duty /= 100;
+  t    /= 2*pi;
 
-  t /= 2*pi;
   v = ones(size(t));
   v(t-floor(t) >= duty) = -1;
 
