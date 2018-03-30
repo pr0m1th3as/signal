bc1ef7f207198e55b73edfd597f001b35fe5cdf7
Mike Miller <mtmiller@users.noreply.github.com>
2018-03-29 17:25:07 -0700

    maint: complete addition of peak2rms and rms
    * INDEX (Signal Measurement): Include peak2rms and rms in new section.
    Move findpeaks from Utility section.
    * peak2rms.m: Reformat GPL version 3 license header, doc string, and input
    validation.  Add input validation %!error tests.
    * rms.m: Likewise.

--- inst/peak2rms.m
+++ inst/peak2rms.m
@@ -1,20 +1,21 @@
 ## Copyright (C) 2015 Andreas Weber <octave@tech-chat.de>
 ##
-## This program is free software; you can redistribute it and/or modify it under
-## the terms of the GNU General Public License as published by the Free Software
-## Foundation; either version 3 of the License, or (at your option) any later
-## version.
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 ##
-## This program is distributed in the hope that it will be useful, but WITHOUT
-## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-## details.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
 ##
-## You should have received a copy of the GNU General Public License along with
-## this program; if not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING. If not, see
+## <https://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{y} =} peak2rms (@var{x})
+## @deftypefn  {Function File} {@var{y} =} peak2rms (@var{x})
 ## @deftypefnx {Function File} {@var{y} =} peak2rms (@var{x}, @var{dim})
 ##
 ## Return the ratio of the largest absolute value to the root-mean-square
@@ -28,15 +29,21 @@
 
 function y = peak2rms (x, dim)
 
+  if (nargin < 1 || nargin > 2)
+    print_usage ();
+  endif
+
   if (nargin == 1)
     y = max (abs (x)) ./ sqrt (meansq (x));
-  elseif (nargin == 2)
-    y = max (abs (x), [], dim) ./ sqrt (meansq (x, dim));
   else
-    print_usage ();
+    y = max (abs (x), [], dim) ./ sqrt (meansq (x, dim));
   endif
 
 endfunction
 
 %!assert (peak2rms ([-2 3; 4 -2]), [4/sqrt(10), 3/sqrt((9+4)/2)])
 %!assert (peak2rms ([-2 3; 4 -2], 2), [3/sqrt((9+4)/2); 4/sqrt(10)])
+
+## Test input validation
+%!error peak2rms ()
+%!error peak2rms (1, 2, 3)
