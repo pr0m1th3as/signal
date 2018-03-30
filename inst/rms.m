089b897ba95e4142a5ec568d8f7d44547ebc590f
Mike Miller <mtmiller@octave.org>
2018-03-29 17:25:07 -0700

    maint: complete addition of peak2rms and rms
    * INDEX (Signal Measurement): Include peak2rms and rms in new section.
    Move findpeaks from Utility section.
    * peak2rms.m: Reformat GPL version 3 license header, doc string, and input
    validation.  Add input validation %!error tests.
    * rms.m: Likewise.

--- inst/rms.m
+++ inst/rms.m
@@ -1,20 +1,21 @@
 ## Copyright (C) 2015 Andreas Weber <octave@tech-chat.de>
 ##
-## This program is free software; you can redistribute it and/or modify it under
-## the terms of the GNU General Public License as published by the Free Software
-## Foundation; either version 3 of the License, or (at your option) any later
-## version.
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 ##
-## This program is distributed in the hope that it will be useful, but WITHOUT
-## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-## details.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
 ##
-## You should have received a copy of the GNU General Public License along with
-## this program; if not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING. If not, see
+## <https://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{y} =} rms (@var{x})
+## @deftypefn  {Function File} {@var{y} =} rms (@var{x})
 ## @deftypefnx {Function File} {@var{y} =} rms (@var{x}, @var{dim})
 ##
 ## Return the root-mean-square (RMS) of @var{x}.
@@ -41,7 +42,7 @@
 
 function y = rms (varargin)
 
-  if (nargin != 1 && nargin != 2)
+  if (nargin < 1 || nargin > 2)
     print_usage ();
   endif
 
@@ -51,3 +52,7 @@ endfunction
 
 %!assert (rms ([1 2 -1]), sqrt (2))
 %!assert (rms ([1 2 -1]'), sqrt (2))
+
+## Test input validation
+%!error rms ()
+%!error rms (1, 2, 3)
