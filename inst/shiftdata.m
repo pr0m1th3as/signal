7c306eeb55a8531781461a1455d460e0d916fcd5
Mike Miller <mtmiller@users.noreply.github.com>
2018-03-29 18:16:18 -0700

    maint: complete addition of shiftdata and unshiftdata
    * shiftdata.m: Reformat GPL version 3 license header and doc string. Rename
    argument to 'dim' for consistency with other functions. Reword input validation
    error messages. Add input validation %!error tests.
    * unshiftdata.m: Likewise.
    * NEWS: Mention them.

--- inst/shiftdata.m
+++ inst/shiftdata.m
@@ -1,50 +1,48 @@
 ## Copyright (C) 2014 Georgios Ouzounis <ouzounis_georgios@hotmail.com>
 ##
-## This file is part of Octave.
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 ##
-## Octave is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or (at
-## your option) any later version.
-##
-## Octave is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not, see
-## <http://www.gnu.org/licenses/>.
+## along with this program; see the file COPYING. If not, see
+## <https://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
 ## @deftypefn  {Function File} {[@var{out perm shifts}] =} shiftdata (@var{in})
-## @deftypefnx {Function File} {[@var{out perm shifts}] =} shiftdata (@var{in}, @var{dimention})
-## Shift data @var{in} to permute the dimention @var{dimention} to the first column.
+## @deftypefnx {Function File} {[@var{out perm shifts}] =} shiftdata (@var{in}, @var{dim})
+## Shift data @var{in} to permute the dimension @var{dim} to the first column.
 ## @seealso{unshiftdata}
 ## @end deftypefn
 
-function [out perm shifts] = shiftdata (in, dimention)
+function [out, perm, shifts] = shiftdata (in, dim)
 
   if (nargin < 1 || nargin > 2)
     print_usage ();
-  elseif (nargin == 1 || (nargin == 2 && isempty (dimention)))
+  elseif (nargin == 1 || (nargin == 2 && isempty (dim)))
     idx = find (size (in) - 1);
-    dimention = idx(1);
+    dim = idx(1);
     shiftsflag = true;
   else
     shiftsflag = false;
   endif
 
-  if (dimention != fix (dimention))
-    error ("shiftdata : DIMENTION must be an integer.");
+  if (dim != fix (dim))
+    error ("shiftdata: DIM must be an integer");
   endif
 
-  perm = [dimention 1: (dimention - 1) (dimention + 1): (length (size (in)))];
+  perm = [dim 1: (dim - 1) (dim + 1): (length (size (in)))];
   out = permute (in, perm);
 
   if (shiftsflag == true)
     perm = [];
-    shifts = dimention - 1;
+    shifts = dim - 1;
   else
     shifts = [];
   endif
@@ -53,14 +51,14 @@ endfunction
 
 %!test
 %! X = [1 2 3; 4 5 6; 7 8 9];
-%! [Y perm shifts] = shiftdata (X, 2);
+%! [Y, perm, shifts] = shiftdata (X, 2);
 %! assert (Y, [1 4 7; 2 5 8; 3 6 9]);
 %! assert (perm, [2 1]);
 
 %!test
 %! X = [27 42 11; 63 48 5; 67 74 93];
 %! X(:, :, 2) = [15 23 81; 34 60 28; 70 54 38];
-%! [Y perm shifts] = shiftdata(X, 2);
+%! [Y, perm, shifts] = shiftdata(X, 2);
 %! T = [27 63 67; 42 48 74; 11 5 93];
 %! T(:, :, 2) = [15 34 70; 23 60 54; 81 28 38];
 %! assert(Y, T);
@@ -68,7 +66,7 @@ endfunction
 
 %!test
 %! X = fix (rand (4, 4, 4, 4) * 100);
-%! [Y perm shifts] = shiftdata (X, 3);
+%! [Y, perm, shifts] = shiftdata (X, 3);
 %! T = 0;
 %! for i = 1:3
 %!   for j = 1:3
@@ -80,3 +78,8 @@ endfunction
 %!   endfor
 %! endfor
 %! assert (T, zeros (size (T)));
+
+## Test input validation
+%!error shiftdata ()
+%!error shiftdata (1, 2, 3)
+%!error shiftdata (1, 2.5)
