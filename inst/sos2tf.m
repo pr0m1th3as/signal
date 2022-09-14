098ca0463a7e3d48d29d8dd54d4eafce001f0468
John Donoghue <john.donoghue@ieee.org>
2022-09-14 08:09:49 -0400

    * inst/sos2tf.m: only removes trailing zeros which are present both in numerator and in denominator (Bug #60597)

--- inst/sos2tf.m
+++ inst/sos2tf.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2005 Julius O. Smith III <jos@ccrma.stanford.edu>
+## Copyright (C) 2021 Charles Praplan
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -17,7 +18,7 @@
 ## -*- texinfo -*-
 ## @deftypefn  {Function File} {[@var{b}, @var{a}] =} sos2tf (@var{sos})
 ## @deftypefnx {Function File} {[@var{b}, @var{a}] =} sos2tf (@var{sos}, @var{g})
-## Convert series second-order sections to direct form @math{H(z) = B(z)/A(z)}.
+## Convert series second-order sections to transfer function.
 ##
 ## INPUTS:
 ## @itemize
@@ -28,8 +29,13 @@
 ## @var{sos} = [@var{B1}.' @var{A1}.'; ...; @var{BN}.' @var{AN}.']
 ## @end example
 ## where
-## @code{@var{B1}.' = [b0 b1 b2] and @var{A1}.' = [1 a1 a2]} for
-## section 1, etc.  The b0 entry must be nonzero for each section.
+## @code{@var{B1}.' = [b0 b1 b2] and @var{A1}.' = [a0 a1 a2]} for
+## section 1, etc.
+##
+## a0 is usually equal to 1 because all 2nd order transfer functions
+## can be scaled so that a0 = 1.
+## However, this is not mandatory for this implementation, which supports
+## all kinds of transfer functions, including first order transfer functions.
 ## See @code{filter} for documentation of the second-order direct-form filter
 ## coefficients @var{B}i and @var{A}i.
 ##
@@ -40,7 +46,8 @@
 ## @end itemize
 ##
 ## RETURNED:
-## @var{b} and @var{a} are vectors specifying the digital filter @math{H(z) = B(z)/A(z)}.
+## @var{b} and @var{a} are vectors specifying the analog or digital filter
+## @math{H(s) = B(s)/A(s)} or @math{H(z) = B(z)/A(z)}.
 ## See @code{filter} for further details.
 ##
 ## @seealso{tf2sos, zp2sos, sos2pz, zp2tf, tf2zp}
@@ -66,16 +73,26 @@ function [B,A] = sos2tf(sos, g = 1)
     A = conv(A, sos(i,4:6));
   endfor
 
-  nB = length(B);
-  while nB && B(nB)==0
-    B=B(1:nB-1);
-    nB=length(B);
+  nA = length (A);
+  nB = length (B);
+  if nA ~= nB
+     error('Internal error: length (A) not equal to length (B)');
+     # This error cannot occur (if the above calls to the conv function
+     # are not modified and if the conv function is also not modified)
+  endif;
+
+  # Removing trailing zeros if present in numerator and denominator
+  while nB && B(nB)==0 && A(nB)==0
+    B = B(1:nB-1);
+    A = A(1:nA-1);
+    nB = length (B);
   endwhile
 
-  nA = length(A);
-  while nA && A(nA)==0
-    A=A(1:nA-1);
-    nA=length(A);
+  # Removing leading zeros if present in numerator and denominator
+  while nB && B(1)==0 && A(1)==0
+    A = A(2:end);
+    B = B(2:end);
+    nB--;
   endwhile
 
   B = B .* prod (g);
@@ -120,3 +137,44 @@ endfunction
 %! assert (Bh, 8 * B, 10*eps);
 %! assert (Ah, A, 10*eps);
 
+## Test with trailing zero in numerator
+%!test
+%! sos = [1, 1, 0, 0, 1, 0.5];
+%! [Bh, Ah] = sos2tf (sos);
+%! assert (Bh, sos(1,1:3) , 10*eps);
+%! assert (Ah, sos(1,4:6), 10*eps);
+
+## Test with trailing zero in denominator
+%!test
+%! sos = [0, 1, 1, 1, 0.5, 0];
+%! [Bh, Ah] = sos2tf (sos);
+%! assert (Bh, sos(1,1:3) , 10*eps);
+%! assert (Ah, sos(1,4:6), 10*eps);
+
+## Test with trailing zero both in numerator and in denominator
+%!test
+%! sos = [1, 1, 0, 1, 0.5, 0];
+%! [Bh, Ah] = sos2tf (sos);
+%! assert (Bh, [1, 1] , 10*eps);
+%! assert (Ah, [1, 0.5], 10*eps);
+
+## Test with leading zero in numerator
+%!test
+%! sos = [0, 1, 1, 1, 1, 0.5];
+%! [Bh, Ah] = sos2tf (sos);
+%! assert (Bh, sos(1,1:3) , 10*eps);
+%! assert (Ah, sos(1,4:6), 10*eps);
+
+## Test with leading zero in denominator
+%!test
+%! sos = [1, 1, 0, 0, 1, 0.5];
+%! [Bh, Ah] = sos2tf (sos);
+%! assert (Bh, sos(1,1:3) , 10*eps);
+%! assert (Ah, sos(1,4:6), 10*eps);
+
+## Test with leading zero both in numerator and in denominator
+%!test
+%! sos = [0, 1, 1, 0, 1, 0.5];
+%! [Bh, Ah] = sos2tf (sos);
+%! assert (Bh, [1, 1] , 10*eps);
+%! assert (Ah, [1, 0.5], 10*eps);
